72fd5fa74c7be371dfba6cd6058caeb876fe1292
JosephTheDataGuy <josephtorres@utexas.edu>
2024-02-24 13:42:47 -0600

    saving local changes before merge

--- read_me.py
+++
@@ -1,5 +0,0 @@
-# This project is for studying stocks
-# 
-# 
-print('This program is intended to mimic stock market websites and utilize SEC data.')
-print('The end program will also include news from different APIs.')
