c6c05d95763a6a8270f7db00ac1c9480bf61d285
JosephTheDataGuy <josephtorres@utexas.edu>
2024-04-29 23:30:11 -0500

    changed project scope to use postgres server instead of AWS RDS hosted MySQL. Added to README to detail and ajusted Main.py. 2 tables have successfully been stored. I have switched to the full query pull for the alpha vantage API instead of compact. In addition, I changed to connection to the server to be minimal as needed, as opposed to being at the beggining of the code to reduce resources needed for the program.

--- Main.py
+++ Main.py
@@ -11,47 +11,61 @@ import re
 
 def main():
     #gathering all necessary secrets as variables from seperate file not stored in git.
-    Alpha_vantage_API_key, News_API_Key, rds_username, rds_password, rds_endpoint, rds_hostname, rds_database_name = gather_keys()
-    
-    #connecting to rds database
-    try:
-        connection = pymysql.connect(host=rds_endpoint,user=rds_username,password=rds_password)
+    Alpha_vantage_API_key, News_API_Key, pg_username, pg_password, pg_host, pg_port, pg_database_name = gather_keys()
+
+    #getting user input to retrieve ticker symbol data from alpha vantage api
+    ticker_data, symbol = get_ticker_data(Alpha_vantage_API_key)
+
+    #save ticker_date to downloads folder incase of server connection fail. 
+    json_file_path = write_dataframe_to_json(ticker_data, f"{symbol}.json")
+    print(f"DateFarame saved to JSON file: {json_file_path}")
+
+    # Connect to PostgreSQL database
+    with connect_to_postgres(pg_username, pg_password, pg_host, pg_port, pg_database_name) as connection:
         cursor = connection.cursor()
-        print("Connected to MySQL database successfuly!")
 
-        #testing connection by displaying mysql version.
-        cursor.execute('select version()')
-        mysql_version = cursor.fetchone()[0]
-        print(f'MySQL version: {mysql_version}')
+        # Storing symbol data into PostgreSQL
+        store_data_in_postgres(cursor, ticker_data, symbol)
 
-        #creating database 'stocks' on MySQL server.
-        # create_database(cursor)
+def connect_to_postgres(username, password, host, port, database_name):
+    """
+    Connects to the PostgreSQL database and returns the connection object.
+    """
+    connection = psycopg2.connect(user=username,
+                                  password=password,
+                                  host=host,
+                                  port=port,
+                                  database=database_name)
+    print("Connected to PostgreSQL database successfully!")
+    return connection
 
-        # connecting to the stocks database in the MySQL server.
-        sql_stocks_db_connection_string = 'USE stocks'
-        cursor.execute(sql_stocks_db_connection_string)
+def store_data_in_postgres(cursor, df, symbol):
+    """
+    This function stores the provided pandas DataFrame (`df`) into a table named
+    after the ticker symbol (`symbol`) in the connected PostgreSQL database (`cursor`).
 
-        #getting user input to retrieve ticker symbol data from alpha vantage api
-        ticker_data, symbol = get_ticker_data(Alpha_vantage_API_key)
-        #storing symbol data into rds
-        store_data_in_mysql(cursor, ticker_data, symbol)
+    Args:
+        cursor: A cursor for the PostgreSQL database.
+        df: The pandas DataFrame containing the data to be stored.
+        symbol: The ticker symbol to be used for the table name.
+    """
 
-        #committing the transaction and closing the connection
-        connection.commit()
-        connection.close()
+    # Generate the table name dynamically
+    table_name = f"{symbol}_alpha_vantage"
+    try:
+        # Check if the table exists
+        cursor.execute("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = %s)", (table_name,))
+        table_exists = cursor.fetchone()[0]
+
+        if not table_exists:
+            # If the table doesn't exist, create it based on the DataFrame columns
+            create_table(cursor, df, table_name)
+
+        # Insert data into the table
+        insert_data(cursor, df, table_name)
+        print("Data stored successfully in PostgreSQL database!")
     except Exception as error:
-        print(error)
-
-def create_database(cursor):
-    sql = 'CREATE DATABASE IF NOT EXISTS stocks'
-    cursor.execute(sql)
-    # check if the database was created successfully
-    cursor.execute("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = %s", ('stocks',))
-    database_exists = cursor.fetchone() is not None
-    if database_exists:
-        print(f'Database stocks exists.')
-    else:
-        print('Database stocks does not exist.')
+        print("Error storing data in PostgreSQL database:", error)
 
 def get_ticker_data(api_key):
     """
@@ -62,7 +76,7 @@ def get_ticker_data(api_key):
         api_key: api key for the alpha vantage apis.
     """
     symbol = input('Ticker : ')
-    outputsize = 'compact'
+    outputsize = 'full'
     ts = TimeSeries(key = api_key, output_format='pandas')
     time_interval = input('Choose one of the following numbers corresponding with the desired time intervals: \n1. Daily (premium)\n2. Weekly\n3. Monthly\n4. 1 minute\n5. 5 minutes\n6. 15 minutes\n7. 30 minutes\n8. 60 minutes\n')
     if time_interval == '1':
@@ -92,7 +106,7 @@ def get_ticker_data(api_key):
     # Add 'symbol' and 'time_zone' columns to the DataFrame
     data['symbol'] = symbol_value
     data['time_zone'] = time_zone_value
-
+    data.reset_index(inplace=True)
     # Sanitize column names to remove leading digits and whitespace
     # We iterate over each column name in the DataFrame 'data.columns'
     # The regular expression pattern r'^\d+\.\s*' matches any sequence of digits followed by a dot and optional whitespace at the beginning of a string
@@ -100,55 +114,38 @@ def get_ticker_data(api_key):
     # The re.sub() function is used to replace any matches of the pattern with an empty string, effectively removing the leading digits and dot
     # After applying this operation to all column names, we get sanitized column names without the leading digits and dot
     # These sanitized column names are then assigned back to the DataFrame 'state.columns' as to avoid future issues when inserting into MySQL.
-    data.columns = [re.sub(r'^\d+\.\s*', '', col) for col in data.columns]
+    data.columns = [re.sub(r'^\d+\.\s*', '', col).replace(' ', '_') for col in data.columns]
 
     print(data)
     #debugging print statement to check the type of the state variable
     print("Type of 'data' variable:", type(data))
     return pd.DataFrame(data), symbol
 
-def store_data_in_mysql(cursor, df, symbol):
+def create_table(cursor, df, table_name):
     """
-    This function stores the provided pandas dataframe (`df`) into a table named
-    after the ticker symbol (`symbol`) in the connected MySQL database (`cursor`).
-
-    Args:
-          cursor: A cursor for the MySQL database.
-        df: The pandas dataframe containing the data to be stored.
-        symbol: The ticker symbol to be used for the table name.
+    Creates a table in the database based on the DataFrame columns.
     """
+    # Sample create_table_query example:
+    # If df.columns contains ['date', 'open', 'high', 'low', 'close', 'volume'], and the
+    # corresponding SQL types are ['VARCHAR(255)', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT', 'INT'],
+    # then the final create_table_query would look like:
+    # "CREATE TABLE symbol_alpha_vantage (date VARCHAR(255), open FLOAT, high FLOAT,
+    # low FLOAT, close FLOAT, volume INT)"
+    create_table_query = f"CREATE TABLE {table_name} ("
+    for column_name in df.columns:
+        dtype = df[column_name].dtype
+        sql_type = get_sql_type(dtype)
+        create_table_query += f"{column_name} {sql_type}, "
+    create_table_query = create_table_query[:-2] + ")"
+    cursor.execute(create_table_query)
+    print(f"Table '{table_name}' created successfully!")
 
-    # Generate the table name dynamically
-    table_name = f"{symbol}_alpha_vantage"
-    try:
-        # Check if the table exists
-        cursor.execute("SHOW TABLES LIKE %s", (table_name,))
-        table_exists = cursor.fetchone() is not None
-
-        if not table_exists:
-                # If the table doesn't exist, create it based on the DataFrame columns
-
-                # Sample create_table_query example:
-                    # If df.columns contains ['date', 'open', 'high', 'low', 'close', 'volume'], and the
-                    # corresponding SQL types are ['VARCHAR(255)', 'FLOAT', 'FLOAT', 'FLOAT', 'FLOAT', 'INT'],
-                    # then the final create_table_query would look like:
-                    # "CREATE TABLE symbol_alpha_vantage (date VARCHAR(255), open FLOAT, high FLOAT,
-                    # low FLOAT, close FLOAT, volume INT)"
-                create_table_query = f"CREATE TABLE {table_name} ("
-                for column_name in df.columns:
-                    dtype = df[column_name].dtype
-                    sql_type = get_sql_type(dtype)
-                    create_table_query += f"{column_name} {sql_type}, "
-                create_table_query = create_table_query[:-2] + ")"
-                cursor.execute(create_table_query)
-                print(f"Table '{table_name}' created successfully!")
-
-        # Insert data into the table
-        insert_query = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({', '.join(['%s']*len(df.columns))})"
-        cursor.executemany(insert_query, df.values.tolist())
-        print("Data stored successfully in MySQL database!")
-    except Exception as error:
-        print("Error storing data in MySQL database:", error)
+def insert_data(cursor, df, table_name):
+    """
+    Inserts data into the specified table.
+    """
+    insert_query = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({', '.join(['%s']*len(df.columns))})"
+    cursor.executemany(insert_query, df.values.tolist())
 
 def get_sql_type(dtype):
     if dtype == 'int64':
@@ -160,6 +157,27 @@ def get_sql_type(dtype):
     else:
         return 'VARCHAR(255)'  # Default to VARCHAR for unsupported types
 
+def write_dataframe_to_json(df, file_name):
+    """
+    Write a pandas DataFrame to a JSON file.
+
+    Args:
+        df (pandas.DataFrame): The DataFrame to be written to JSON.
+        file_name (str): The name of the JSON file.
+
+    Returns:
+        str: The full path to the saved JSON file.
+    """
+    # Get the path to the Downloads folder
+    downloads_folder = os.path.join(os.path.expanduser('~'), 'Downloads')
+
+    # Construct the full file path
+    file_path = os.path.join(downloads_folder, file_name)
+
+    # Write the DataFrame to JSON
+    df.to_json(file_path, orient='records')
+
+    return file_path
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Stocks
 The purpose of this repo is to create a resource that utilizes SEC data and various news API to pull pertanent data regarding user input of a ticker symbol.
+
+Originally, the project was designed to use AWS RDS and a MySQL hosted database. After successfully setting it up, I was hit with a $1.80 bill for the month, having exceeded the free tier. This bill scared me from using the cloud for the service as I do not really have a budget for this project. Therefore, I swithced to using PostgresSQL 16 hosted on my local machine for my database.
+
+Moving forward, the goal is to set up a system to pull a ticker symbol's data dependent on user input, and set up a cron job to automatically pull and store the data on a daily basis.
+
